d776571d6673dcd335889c8fcf6eaefdea5bd2cc
CleymerM1 <cleymer.mendoza@unah.hn>
2022-06-21 10:49:45 -0600

    Se agrego el landing page

--- Frontend/KAPEKSHOP/src/app/app-routing.module.ts
+++ Frontend/KAPEKSHOP/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './Components/inicio/inicio.component';
+import { LandingComponent } from './Components/landing/landing.component';
 import { ListarProductosComponent } from './Components/listar-productos/listar-productos.component';
 import { RegistrarUsuarioComponent } from './Components/registrar-usuario/registrar-usuario.component';
 
 //componentes
 const routes: Routes = [
-  {path: '', component:InicioComponent},
+  {path: '', component:LandingComponent},
+  {path: 'inicio', component:InicioComponent},
   {path:'registrar-usuario',component:RegistrarUsuarioComponent},
   {path:'editar-usuario/:id', component:RegistrarUsuarioComponent},
   {path:'listar-productos', component:ListarProductosComponent},
--- Frontend/KAPEKSHOP/src/app/app.module.ts
+++ Frontend/KAPEKSHOP/src/app/app.module.ts
@@ -7,18 +7,28 @@ import { RegistrarUsuarioComponent } from './Components/registrar-usuario/regist
 import { InicioComponent } from './Components/inicio/inicio.component';
 import { ListarProductosComponent } from './Components/listar-productos/listar-productos.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LandingComponent } from './Components/landing/landing.component';
+import { HeaderComponent } from './Components/landing/header/header.component';
+import { BodyComponent } from './Components/landing/body/body.component';
+import { FooterComponent } from './Components/landing/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegistrarUsuarioComponent,
     InicioComponent,
-    ListarProductosComponent
+    ListarProductosComponent,
+    LandingComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    NgbModule,
     
   ],
   providers: [],
