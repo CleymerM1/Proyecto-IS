2e43e53bce8aebf8a52428425fa6b922e241ce23
kerenbm <82061667+kerenbm@users.noreply.github.com>
2022-06-24 14:54:16 -0600

    Update usuario.service.ts

--- Frontend/KAPEKSHOP/src/app/Services/usuario.service.ts
+++ Frontend/KAPEKSHOP/src/app/Services/usuario.service.ts
@@ -1,15 +1,23 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
+  url = 'http://localhost:3000/usuario/registro/';
 
-  url='/usuario/registro'
-  constructor(private http: HttpClient) { }
+  constructor(private http:HttpClient) { }
 
-  getUsuario(){
+
+  crearUsuario(objUsuario:any): Observable<any> {
+    let urlStr = `http://localhost:3000/usuario/registro/`
+      return this.http.post(urlStr, objUsuario)
+  }
+
+  getUsuarios(): Observable<any>{
+    
     return this.http.get(this.url);
   }
 }
