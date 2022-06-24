1d0303185881ef759fff1820d0e99e134e37ba5f
kerenbm <82061667+kerenbm@users.noreply.github.com>
2022-06-24 14:42:17 -0600

    Update usuarioController.js

--- Backend/controllers/usuarioController.js
+++ Backend/controllers/usuarioController.js
@@ -14,22 +14,25 @@ exports.crearUsuario = (req,res)=>{
         contraseña: req.body.contraseña,
         telefono: req.body.telefono
     })
-    if(this.verificarCorreoUsuario(usuario.correo, res)){
-        Usuario.crear(usuario, (err, data)=>{
-            if(err){
-                console.log(err)
-                res.status(500).send({
-                    mensaje: err.mensaje || "Error al guardar en la base de datos",
-                    error: err
-                });
-    
-            }
-            //console.log(data);
-        }) 
-    }
-    res.json('Correo existente');
-    console.log('No se pudo agregar el usuario');
-       
+
+
+    Usuario.crear(usuario, (err, data)=>{
+        if(err){
+
+            return res.status(500).send({
+                mensaje: err.mensaje || "Error al guardar en la base de datos",
+                error: err
+            });
+        }
+
+        if(data.error){
+            console.log(data)
+            return res.status(502).send({msj: data.error})
+        }else{
+            return res.status(200).json({msj:'El usuario se registro correctamente'})
+
+        }
+    })    
 };
 
 exports.obtenerUsuario = (req,res)=>{
@@ -39,18 +42,6 @@ exports.obtenerUsuario = (req,res)=>{
         }else{
             res.status(200).json(data);
         }
-    });
-};
-
-exports.verificarCorreoUsuario = (req,res)=>{
-    
-    Usuario.verificarCorreo(req,(error,data)=>{
-        if(error){
-            res.status(404).json({mensaje: 'No se pudieron obtener los datos'})
-        }
-        //console.log(res.json(data));
-        //console.log(data);
-        return data;
     });
 };
 
