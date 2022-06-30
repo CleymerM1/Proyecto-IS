67bf46e3cb4dd263c651e2dfe2cd15a9ce400567
kerenbm <82061667+kerenbm@users.noreply.github.com>
2022-06-29 20:24:56 -0600

    Update usuario.js

--- Backend/models/usuario.js
+++ Backend/models/usuario.js
@@ -1,5 +1,8 @@
 /*-------------------------------Import para la conexion con la base de datos------------------------------*/ 
 const conexion = require('../config/conexion');
+const jwt = require('jsonwebtoken');
+const correoServicio = require('../helpers/email.js')
+
 /*------------------------------------------Creacion de clases---------------------------------------------*/ 
 const Usuario = function(objUsuario){
     this.nombre = objUsuario.nombre;
@@ -13,11 +16,12 @@ const Usuario = function(objUsuario){
 /*---------------------------------------------Funciones---------------------------------------------------*/
 Usuario.crear = (newObjUsuario, res)=>{
     let buscarCorreo = `select correo from usuario where correo='${newObjUsuario.correo}'`
-    let insertQuery = `insert into usuario (idRol, nombre, apellido, correo, direccion, departamento, contrasenia, estado, telefono) 
-                                            VALUES (2, '${newObjUsuario.nombre}', '${newObjUsuario.apellido}', '${newObjUsuario.correo}',
-                                            '${newObjUsuario.direccion}','${newObjUsuario.departamento}','${newObjUsuario.contraseña}','activado','${newObjUsuario.telefono}')`;
+    let token = jwt.sign({correo: newObjUsuario.correo},process.env.JWT_SECRET)
+    //let insertQuery = `insert into usuario (idRol, nombre, apellido, correo, direccion, departamento, contrasenia, estado, telefono) VALUES (2, '${newObjUsuario.nombre}', '${newObjUsuario.apellido}', '${newObjUsuario.correo}','${newObjUsuario.direccion}','${newObjUsuario.departamento}','${newObjUsuario.contraseña}','activado','${newObjUsuario.telefono}')`;
+    let insertQuery = `insert into usuario (idRol, nombre, apellido, correo, direccion, departamento, contrasenia, estado, telefono, token) VALUES (2, '${newObjUsuario.nombre}', '${newObjUsuario.apellido}', '${newObjUsuario.correo}','${newObjUsuario.direccion}','${newObjUsuario.departamento}',AES_ENCRYPT('${newObjUsuario.contraseña}','${newObjUsuario.contraseña}'),'activado','${newObjUsuario.telefono}', '${token}')`;
 
     conexion.query(buscarCorreo, (err, resUsuario)=>{
+
         if(err){
             return res(err, null)
         }
@@ -25,11 +29,31 @@ Usuario.crear = (newObjUsuario, res)=>{
         if(resUsuario.length) {
             return res(null, {error: 'El correo ya esta en uso', tipo: 'no_permitido'})
         }else {
-            conexion.query(insertQuery, (err, resRegistrarUsuario) => {
-                
-                if (err) return res({msj: 'El usuario no pudo ser registrado'}, null)
 
-                return res(null,{msj: 'El usuario fue registrado correctamente'})
+            
+
+            conexion.query(insertQuery, async (err, resRegistrarUsuario) => {
+
+                console.log(err)
+                if (err) return res({msj: 'El usuario no pudo ser registrado'}, null)
+                let data = {
+                    correo: newObjUsuario.correo,
+                    nombre: newObjUsuario.nombre,
+                    token: token
+                }
+
+                // Enviar correo al usuario para verificar su cuenta
+                try {
+                    await correoServicio(data)
+                    return res(null,{msj: 'Se envio un correo con las instrucciones para válidar tu cuenta.'})
+                } catch (error) {
+                    console.log(error)
+                    return res({msj: 'No se pudo enviar el correo al usuario'},null)
+                    // Borrar el usuario de la base de datos
+                    
+                }
+
+
             })
         }
     });
@@ -42,6 +66,54 @@ Usuario.obtener = (resultado)=>{
 
     });
 };
+
+Usuario.validarTokenRegistro = (req, resultado) => {
+    let { token } = req.body
+    console.log(req.body)
+    let consulta = `SELECT * FROM usuario WHERE token = '${token}'`
+    let consultaActualizarToken = `UPDATE usuario SET token = '' WHERE token= '${token}'`;
+    conexion.query(consulta, (err, res) => {
+        if (res.length) {
+            // Borrar el token del usuario 
+            conexion.query(consultaActualizarToken, (error, resp) => {
+                if(error){
+                    return resultado(err, null)
+                }else {
+                    return resultado(null, res)
+                }
+               
+            })
+        }else {
+            return resultado(err, null)
+        }
+    })
+};
+
+Usuario.iniciarSeion = (resultado)=>{
+    let consulta = `SELECT * FROM usuario where correo = '${req.body.correo}' and contrasenia = AES_ENCRYPT('${req.body.contrasenia}','${req.body.contrasenia}')`
+    conexion.query(consulta, (err, resUsuario) => {
+        if(err) return respuesta(err, null)
+        
+
+        return respuesta(null, resUsuario )
+   
+    } )
+};
+
+Usuario.obtenerPorCorreo = ( req, respuesta ) => {
+
+    let correo = req.params ? req.params.correo : req
+    let buscarCorreo = `select idUsuario, idRol, nombre, apellido, correo, direccion, departamento, estado, telefono from usuario where correo='${correo}'`
+    conexion.query(buscarCorreo, (err, resUsuario) => {
+        if(err) {
+            return respuesta(err, null)
+        }
+        return respuesta(null, resUsuario)
+    
+    })
+};
+
+
 
 
 module.exports = Usuario;
